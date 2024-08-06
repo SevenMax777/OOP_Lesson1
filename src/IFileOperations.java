b74e0d16f3e39a9f2d74754c363047c76e14f802
Maksim Ovchinnikov <seven.85@list.ru>
2024-08-06 11:07:08 +0500

    Lesson_2

---
+++ src/IFileOperations.java
@@ -0,0 +1,9 @@
+public class IFileOperations {
+    
+}
+import java.io.IOException;
+
+public interface IFileOperations {
+    void saveToFile(String fileName) throws IOException;
+    void loadFromFile(String fileName) throws IOException, ClassNotFoundException;
+}
